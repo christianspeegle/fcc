29edf6af1e7a81b4533f69db779c3fe185dd0af9
Christian Speegle <christian@christianspeegle.com>
2020-09-29 00:45:52 -0500

    added parsing middleware and added name in app.listen log

--- app/headers.js
+++ app/headers.js
@@ -7,7 +7,13 @@ const port = 6002
 app.use(cors({optionsSuccessStatus: 200}))
 
 app.use("/", (request, response) => {
+	const responseObject = {
+		ipaddress: request.header("X-Forwarded-For"),
+		language: request.header("Accept-Language"),
+		software: request.header("User-Agent")
+	}
 
+	response.json(responseObject)
 })
 
-app.listen(port, () => console.log(`<<Microservice Name>> microservice started on port ${port}`))
+app.listen(port, () => console.log(`Request Header Parser microservice started on port ${port}`))
