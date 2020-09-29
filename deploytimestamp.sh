2e9b9a640d71ce191096dc7224ded9d1d90eea5c
Christian Speegle <christian@christianspeegle.com>
2020-09-28 23:06:51 -0500

    changed zip command to only zip toplevel files of the ./timestamp directory so node_modules is not also zipped

--- deploytimestamp.sh
+++ deploytimestamp.sh
@@ -2,7 +2,7 @@ if [ -f "timestamp.zip" ]; then
   rm timestamp.zip
 fi
 
-zip timestamp.zip timestamp/*
+find ./timestamp -maxdepth 1 -type f -print0 | xargs -0 zip timestamp.zip
 scp timestamp.zip bastion@bastionofshadows.com:
 ssh bastion@bastionofshadows.com "rm -r ~/fcc.christianspeegle.com/timestamp"
 ssh bastion@bastionofshadows.com "unzip ~/timestamp.zip -d ~/fcc.christianspeegle.com"
