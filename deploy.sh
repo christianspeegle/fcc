33159dbbcc61931f08bf257d5983f70893d5a0ee
Christian Speegle <christian@christianspeegle.com>
2020-09-29 12:38:16 -0500

    updated server npm i call to only install production dependencies

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ find ./app -type f -print0 | xargs -0 zip app.zip
 scp app.zip bastion@bastionofshadows.com:
 ssh bastion@bastionofshadows.com "rm -r ~/fcc.christianspeegle.com"
 ssh bastion@bastionofshadows.com "unzip ~/app.zip -d ~/fcc.christianspeegle.com"
-ssh bastion@bastionofshadows.com "npm --prefix ~/fcc.christianspeegle.com i"
+ssh bastion@bastionofshadows.com "npm --prefix ~/fcc.christianspeegle.com i --only=production"
 ssh bastion@bastionofshadows.com "rm ~/app.zip"
