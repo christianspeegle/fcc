c835f1c1a0f510f429785f5c459b3202847287db
Christian Speegle <christian@christianspeegle.com>
2020-09-28 23:38:32 -0500

    improved zip command to exclude node_modules instead of only zipping toplevel directory (this allows subdirectories to be zipped as well, should they ever be added)

--- deploytimestamp.sh
+++ deploytimestamp.sh
@@ -2,7 +2,7 @@ if [ -f "timestamp.zip" ]; then
   rm timestamp.zip
 fi
 
-find ./timestamp -maxdepth 1 -type f -print0 | xargs -0 zip timestamp.zip
+find ./timestamp -path ./timestamp/node_modules -prune -o -print0 | xargs -0 zip timestamp.zip
 scp timestamp.zip bastion@bastionofshadows.com:
 ssh bastion@bastionofshadows.com "rm -r ~/fcc.christianspeegle.com/timestamp"
 ssh bastion@bastionofshadows.com "unzip ~/timestamp.zip -d ~/fcc.christianspeegle.com"
